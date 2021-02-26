92a42e252939074f9e3efd8a8a53c7b131397295
Norbu Sherpa <norbuts@hotmail.com>
2021-02-26 13:50:20 +1100

    updated folder structure

--- employee.js
+++ employee.js
@@ -1,15 +1,59 @@
+const inqurier = require('inquirer');
+const mysql = require('mysql'),
+const server.js =require('./server')
+
+
+
+
 // function to update employee
-const updateEmployee = () => {
-    connection.query('UP * FROM songs', (err, res) => {
+const addDepartment = () => {
+    connection.query('INSERT into department  ', (err, res) => {
       if (err) throw err;
       console.log(res); // Array of rows of table
-      res.forEach(({ id, title, artist, genre }) => {
+      res.forEach(({  }) => {
         console.log(`${id} | ${title} | ${artist} | ${genre}`);
       });
-      // Same as:
-      res.forEach(result => {
-        console.log(`${result.id} | ${result.title} | ${result.artist} | ${result.genre}`);
-      }) 
       console.log('-----------------------------------');
     });
   };
+
+// function add roles
+
+
+//function to add employees
+
+//fucntion to view all department
+  const viewAllDepartment = () => {
+    connection.query('SELECT * from department', (err, res) => {
+      if (err) throw err;
+      console.log(res); // Array of rows of table
+      res.forEach(({  }) => {
+        console.log(`${id} | ${name} `);
+      });
+      console.log('-----------------------------------');
+    });
+  };
+
+//function to view all roles
+const viewAllRole= () => {
+    connection.query('SELECT * from roles', (err, res) => {
+      if (err) throw err;
+      console.log(res); // Array of rows of table
+      res.forEach(({  }) => {
+        console.log(`${id} | ${title} | ${salary} | ${department_id} `);
+      });
+      console.log('-----------------------------------');
+    });
+  };
+
+//function to view all employees
+
+
+//Function to update employee roles.
+
+
+//function to update employee's manager.
+
+//function to view employees by manager.
+
+//delete department.
